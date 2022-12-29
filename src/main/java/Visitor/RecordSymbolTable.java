ba58d20c70ad1f4e549ef056c17fbcecea6b3896
s.napolitano44 <s.napolitano44@studenti.unisa.it>
2022-12-29 17:08:32 +0100

    bbv

---
+++ src/main/java/Visitor/RecordSymbolTable.java
@@ -0,0 +1,28 @@
+package Visitor;
+
+
+import java.util.ArrayList;
+
+public class RecordSymbolTable {
+    public String nome;
+    public String kind;
+    public ArrayList<String> typeParametri;
+    public ArrayList<String> typeRitorno;
+
+    public RecordSymbolTable(String nome, String kind, ArrayList<String> typeParametri, ArrayList<String> typeRitorno) {
+        this.nome = nome;
+        this.kind = kind;
+        this.typeParametri = typeParametri;
+        this.typeRitorno = typeRitorno;
+    }
+
+    @Override
+    public String toString() {
+        return "RecordSymbolTable{" +
+                "nome=" + nome +
+                ", kind='" + kind + '\'' +
+                ", typeParametri=" + typeParametri +
+                ", typeRitorno=" + typeRitorno +
+                '}';
+    }
+}
