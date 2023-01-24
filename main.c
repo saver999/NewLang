72f9be247fdb5727a05accb40ff265b0852144e6
s.napolitano44 <s.napolitano44@studenti.unisa.it>
2023-01-24 15:33:09 +0100

    risolto alcune cose

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+elaboraScelta(null){floatresult,numB,numA;
+if(){if(){null}else{printf("%s 
+", )}}if(){printf("%s %f 
+", , )null}if(){printf("%s %f 
+", , )null}if(){printf("%s %f 
+", , )null}printf("inserisci il secondo numero");
+scanf("%f",&numB);printf("inserisci il primo numero");
+scanf("%f",&numA);numA = ;
+numB = ;
+result = ;
+}somma(nullnull){result = ;
+}sottrazione(nullnull){result = ;
+}divisione(nullnull){result = ;
+}moltiplicazione(nullnull){result = ;
+}
